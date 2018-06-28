ef42628626c67e188ce20dfe62997b154d605351
Jodie Putrino <j.putrino@f5.com>
2018-06-28 14:56:19 -0600

    Test combining the deployment and invalidation into a single script

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -6,4 +6,9 @@
 # Don't set -x
 # we need to keep the secret AWS variables out of the logs
 
-exec docker run --rm -it -v $PWD:$PWD --workdir $PWD -e  AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_S3_BUCKET=$AWS_S3_BUCKET f5devcentral/containthedocs "$@"
+exec docker run --rm -i -v $PWD:$PWD --workdir $PWD -e  AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_S3_BUCKET=$AWS_S3_BUCKET f5devcentral/containthedocs /bin/bash -s <<EOF
+
+publish-product-docs-to-prod agc v3.0
+aws cloudfront create-invalidation --distribution-id $AWS_DIST_ID --paths /products/agc/v3.0
+
+EOF
