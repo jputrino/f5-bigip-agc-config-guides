508f9865ee471345138ef4328ddda4387adf5261
Jodie Putrino <j.putrino@f5.com>
2018-06-29 09:27:06 -0600

    Update deploy-docs.sh
    Add trailing slash to fix cloudfront invalidation

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -9,6 +9,6 @@
 exec docker run --rm -i -v $PWD:$PWD --workdir $PWD -e  AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_S3_BUCKET=$AWS_S3_BUCKET f5devcentral/containthedocs /bin/bash -s <<EOF
 
 publish-product-docs-to-prod agc v3.0
-aws cloudfront create-invalidation --distribution-id $AWS_DIST_ID --paths /products/agc/v3.0
+aws cloudfront create-invalidation --distribution-id $AWS_DIST_ID --paths /products/agc/v3.0/
 
 EOF
